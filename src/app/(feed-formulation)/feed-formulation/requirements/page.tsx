78f8758285fd49ad11fe076e0a083ffe699f5d19
Kelvin Muza <k1muza@gmail.com>
2026-09-25 23:19:01 +0200

    Redirect legacy requirements route to programmes

--- src/app/(feed-formulation)/feed-formulation/requirements/page.tsx
+++ src/app/(feed-formulation)/feed-formulation/requirements/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation";
 import { feedFormulationHref } from "@/lib/routes";
 
 export default function LegacyRequirementsPage() {
-  redirect(feedFormulationHref("nutrients"));
+  redirect(feedFormulationHref("programmes"));
 }
